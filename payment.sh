de41e1212cf0f84d754bc1876929781458ad68a3
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-30 14:13:13 +0530

    Add new scripts and files

--- payment.sh
+++ payment.sh
@@ -4,6 +4,9 @@ source ./common.sh
 APP_NAME=payment
 
 CHECK_ROOT
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+VALIDATE $? "looking"
+
 APP_SETUP
 
 cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
