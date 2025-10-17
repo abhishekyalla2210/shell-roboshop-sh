e4d225f964732c6de40dc7110ba9e93f2946de0b
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-10-17 16:49:29 +0530

    Add new scripts and files

--- disk-usage.sh
+++ disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2  
 MESSAGE=""
-IP_ADDRESS= $(curl -s http://169.254.169.254/latest/meta-data/local-ip)
+IP_ADDRESS= $(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read -r line
 do
