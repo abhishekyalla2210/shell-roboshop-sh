76cfbffe31c52334884b6449a3839fb849c0b7ac
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-10-17 07:40:49 +0530

    Add new scripts and files

--- backup.sh
+++ backup.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-count=5
- 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USER_ID=$(id -u)
 
-echo -e " starting countdown:"
+LOGS_FOLDER="var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-while [ $count -ge 0 ]
-do
-    echo "count=$count"
-    sleep 1
+mkdir $LOGS_FOLDER
+echo "script started at:$(date)"
 
-    count=$((count - 1))
-    done 
-    echo "times up!"
+SOURCE_DIR=/home/ec2-user/app-logs
+  
+  if [ $USER_ID -ne 0 ]; then
+    echo -e $R "please login with root access $N"
+    exit 1
+fi
+
+USAGE(){
+echo "USAGE: sudo sh backup"
+
+
+}
+
+if [ $# -lt 2 ]; then
+USAGE
+  
