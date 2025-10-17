531c4bf5c566d5cba79d160624d97f4d9cf3fd44
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-10-17 16:46:16 +0530

    Add new scripts and files

--- disk-usage.sh
+++ disk-usage.sh
@@ -3,6 +3,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2  
 MESSAGE=""
+IP_ADDRESS= $(curl -s http://169.254.169.254/latest/meta-data/local-ip)
 
 while IFS= read -r line
 do
@@ -15,3 +16,5 @@ do
     fi
 done <<< $DISK_USAGE
 echo -e "message body: $MESSAGE"
+
+sh mail.sh "abhishekyalla7@gmail.com" "high disk usage alert" "high disk usage" "$MESSAGE" "$IP_ADDRESS" "DEVOPS team"
