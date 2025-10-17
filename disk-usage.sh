37985cc7f5574b60ad48ec890e54bcaa8542fa08
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-10-17 12:34:10 +0530

    Add new scripts and files

---
+++ disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v filesystem)
+DISK_THRESHOLD=2
+
+while IFS= read -r line
+do
+
+    USAGE= $(echo $line | awk '{print $6}'
+| cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE="hgh disk usage of $PARTITION: $USAGE %"
+
+    fi
+done <<< $DISK_USAGE
